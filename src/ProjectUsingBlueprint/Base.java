2a2efbb9734eb5f736528df16de923dc396b3e40
Thurunu Hashitha <106402728+Thurunuhashitha@users.noreply.github.com>
2026-03-20 13:07:10 +0530

    project Using Blue print Using From Original Class

---
+++ src/ProjectUsingBlueprint/Base.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Base {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String choice = "y";
+
+        Vehicle[] vehicles = new Vehicle[10];
+        int count = 0;
+
+        while (choice.equalsIgnoreCase("y")) {
+
+            Vehicle v1 = new Vehicle();
+
+            System.out.print("Enter Vehicle Brand: ");
+            v1.brand = scan.next();
+
+            System.out.print("Enter Vehicle Engine Number: ");
+            v1.enCa = scan.nextInt();
+
+            System.out.print("Enter Vehicle Chacy Number: ");
+            v1.chNo = scan.next();
+
+            System.out.print("Enter Vehicle Color: ");
+            v1.colo = scan.next();
+
+            System.out.print("Enter Vehicle Year: ");
+            v1.year = scan.nextInt();
+
+            vehicles[count] = v1;
+
+            System.out.print("Do you want to add another vehicle? (y/n): ");
+            choice = scan.next();
+        }
+
+        enginAbove1500(vehicles);
+    }
+
+    public static void enginAbove1500(Vehicle[] vehicles) {
+
+        for (int i = 0; i < vehicles.length; i++) {
+            if (vehicles[i].enCa > 1500) {
+                System.out.println("-----------------------------------------");
+                System.out.println("Brand: " + vehicles[i].brand);
+                System.out.println("Engine No: " + vehicles[i].enCa);
+                System.out.println("Chacy No: " + vehicles[i].chNo);
+                System.out.println("Color: " + vehicles[i].colo);
+                System.out.println("Year: " + vehicles[i].year);
+            }
+        }
+    }
+}
